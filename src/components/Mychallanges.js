f4d19d8f96f79bb29fe3b414170468d17bd31dd6
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-09 00:10:41 +0530

    Changing My Challenges Page

--- src/components/Mychallanges.js
+++ src/components/Mychallanges.js
@@ -1,158 +1,73 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 
 const Mychallanges = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const getNoOfPendingReceived = async () => {
+      const token = localStorage.getItem('auth-token');
+      
+      const response = await fetch('http://localhost:5000/api/challenge/getnoofpendingreceivedchallenges', {
+        method: 'GET',
+        headers:{
+          'Content-Type': 'application/json',
+          'auth-token': token
+        }
+      });
+      const json = await response.json();
+      // console.log(json);
+    }
+    getNoOfPendingReceived();
+  }, [])
+  
   return (
     <>
     <Navbar active={"mychallanges"} />
     <div className="container flex flex-col justify-center">
       <h1 className='text-white flex items-center justify-center mt-10 text-3xl font-bold'>My Challenges</h1>
-      <div className="in-progress p-16 flex flex-col">
-        <h2 className='text-white text-xl'>In Progress</h2>
+      <div className="all-challanges md:p-16 p-8 flex flex-col justify-center items-center">
 
-        <div className="card-carousel flex flex-row w-[120%]">
-
-
-
-                {/* <div key={challenge._id} className="card fade-in rounded-3xl my-4 mx-4 bg-[#555555] md:w-[28%] w-full"> */}
-                <div key={{}} className="card fade-in rounded-3xl my-4 bg-[#555555] md:w-[15%] mr-4">
-                  <div className="card-head text-white my-4 text-sm mx-4">
-                    December 10, 2020
-                  </div>
-                  <div className="image flex justify-center my-5">
-                    {/* <img className='rounded-2xl' width={100} src={challenge.challengeByPFPURI} alt="" /> */}
-                    <img className='rounded-2xl' width={100} src={"https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"} alt="" />
-                  </div>
-                  <div className="card-title justify-center items-center my-5 flex flex-col">
-                    <div className="main text-white font-bold">
-                      Abhinandan Wadhwa
-                      {/* {challenge.challengeByName} */}
-                    </div>
-                    <div className="notMain text-sm my-3">
-                        {/* <button onClick={() => openSeeDetails(challenge._id)} className='underline'>See Details</button> */}
-                        <button onClick={{}} className='underline'>See Details</button>
-                    </div>
-                  </div>
-                  <div className="status flex flex-row justify-center my-5">
-                    <button className='text-gray-100 bg-green-600 p-2 mx-2 w-[35%] rounded-lg'>Accept</button>
-                    <button className='text-gray-100 bg-red-500 p-2 mx-2 rounded-lg w-[35%]'>Decline</button>
-                  </div>
-                  <div className="footer flex flex-row justify-between text-[#ffc144] font-bold">
-                    <div className="somerandonimages">
-
-                    </div>
-                    {/* <div className="timeLeft mx-4 my-2 text-[#ffdfb6]">
-                      2 Days Left
-                    </div> */}
-                  </div>
-                </div>
-                {/* <div key={challenge._id} className="card fade-in rounded-3xl my-4 mx-4 bg-[#555555] md:w-[28%] w-full"> */}
-                <div key={{}} className="card fade-in rounded-3xl my-4 bg-[#555555] md:w-[15%] mr-4">
-                  <div className="card-head text-white my-4 text-sm mx-4">
-                    December 10, 2020
-                  </div>
-                  <div className="image flex justify-center my-5">
-                    {/* <img className='rounded-2xl' width={100} src={challenge.challengeByPFPURI} alt="" /> */}
-                    <img className='rounded-2xl' width={100} src={"https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"} alt="" />
-                  </div>
-                  <div className="card-title justify-center items-center my-5 flex flex-col">
-                    <div className="main text-white font-bold">
-                      Abhinandan Wadhwa
-                      {/* {challenge.challengeByName} */}
-                    </div>
-                    <div className="notMain text-sm my-3">
-                        {/* <button onClick={() => openSeeDetails(challenge._id)} className='underline'>See Details</button> */}
-                        <button onClick={{}} className='underline'>See Details</button>
-                    </div>
-                  </div>
-                  <div className="status flex flex-row justify-center my-5">
-                    <button className='text-gray-100 bg-green-600 p-2 mx-2 w-[35%] rounded-lg'>Accept</button>
-                    <button className='text-gray-100 bg-red-500 p-2 mx-2 rounded-lg w-[35%]'>Decline</button>
-                  </div>
-                  <div className="footer flex flex-row justify-between text-[#ffc144] font-bold">
-                    <div className="somerandonimages">
-
-                    </div>
-                    {/* <div className="timeLeft mx-4 my-2 text-[#ffdfb6]">
-                      2 Days Left
-                    </div> */}
-                  </div>
-                </div>
-                {/* <div key={challenge._id} className="card fade-in rounded-3xl my-4 mx-4 bg-[#555555] md:w-[28%] w-full"> */}
-                <div key={{}} className="card fade-in rounded-3xl my-4 bg-[#555555] md:w-[15%] mr-4">
-                  <div className="card-head text-white my-4 text-sm mx-4">
-                    December 10, 2020
-                  </div>
-                  <div className="image flex justify-center my-5">
-                    {/* <img className='rounded-2xl' width={100} src={challenge.challengeByPFPURI} alt="" /> */}
-                    <img className='rounded-2xl' width={100} src={"https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"} alt="" />
-                  </div>
-                  <div className="card-title justify-center items-center my-5 flex flex-col">
-                    <div className="main text-white font-bold">
-                      Abhinandan Wadhwa
-                      {/* {challenge.challengeByName} */}
-                    </div>
-                    <div className="notMain text-sm my-3">
-                        {/* <button onClick={() => openSeeDetails(challenge._id)} className='underline'>See Details</button> */}
-                        <button onClick={{}} className='underline'>See Details</button>
-                    </div>
-                  </div>
-                  <div className="status flex flex-row justify-center my-5">
-                    <button className='text-gray-100 bg-green-600 p-2 mx-2 w-[35%] rounded-lg'>Accept</button>
-                    <button className='text-gray-100 bg-red-500 p-2 mx-2 rounded-lg w-[35%]'>Decline</button>
-                  </div>
-                  <div className="footer flex flex-row justify-between text-[#ffc144] font-bold">
-                    <div className="somerandonimages">
-
-                    </div>
-                    {/* <div className="timeLeft mx-4 my-2 text-[#ffdfb6]">
-                      2 Days Left
-                    </div> */}
-                  </div>
-                </div>
-                {/* <div key={challenge._id} className="card fade-in rounded-3xl my-4 mx-4 bg-[#555555] md:w-[28%] w-full"> */}
-                <div key={{}} className="card fade-in rounded-3xl my-4 bg-[#555555] md:w-[15%] mr-4">
-                  <div className="card-head text-white my-4 text-sm mx-4">
-                    December 10, 2020
-                  </div>
-                  <div className="image flex justify-center my-5">
-                    {/* <img className='rounded-2xl' width={100} src={challenge.challengeByPFPURI} alt="" /> */}
-                    <img className='rounded-2xl' width={100} src={"https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"} alt="" />
-                  </div>
-                  <div className="card-title justify-center items-center my-5 flex flex-col">
-                    <div className="main text-white font-bold">
-                      Abhinandan Wadhwa
-                      {/* {challenge.challengeByName} */}
-                    </div>
-                    <div className="notMain text-sm my-3">
-                        {/* <button onClick={() => openSeeDetails(challenge._id)} className='underline'>See Details</button> */}
-                        <button onClick={{}} className='underline'>See Details</button>
-                    </div>
-                  </div>
-                  <div className="status flex flex-row justify-center my-5">
-                    <button className='text-gray-100 bg-green-600 p-2 mx-2 w-[35%] rounded-lg'>Accept</button>
-                    <button className='text-gray-100 bg-red-500 p-2 mx-2 rounded-lg w-[35%]'>Decline</button>
-                  </div>
-                  <div className="footer flex flex-row justify-between text-[#ffc144] font-bold">
-                    <div className="somerandonimages">
-
-                    </div>
-                    {/* <div className="timeLeft mx-4 my-2 text-[#ffdfb6]">
-                      2 Days Left
-                    </div> */}
-                  </div>
-                </div>
-                
-
-                <div className="see-all-btn flex justify-center items-center">
-                  <button onClick={() => navigate('/')} className="btn bg-[#535b76cf] inline-flex justify-center items-center w-[70px] h-[70px] leading-[50px] bg-blue-800 text-center font-[15px] text-white mx-4 font-bold mt-4 rounded-full">All <AiOutlineArrowRight /></button>
-                </div>
-
-
-
+        <div className="card md:w-[80%] w-[100%] bg-gray-800 rounded-2xl my-2 cursor-pointer">
+        <div style={{alignItems: 'center'}} className="update p-4 flex 2xl:items-start w-full text-white hover:rounded-2xl hover:bg-gray-700">
+          <div className="left flex flex-col">
+            <img src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="Abhi" className='h-10 w-10 rounded-full object-cover' />
+            <p className='ml-2'>vs</p>
+            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" width={50} alt="Monika" className='h-10 w-10 rounded-full object-cover' />
+          </div>
+          <div style={{borderLeft: '2px solid #8080805e', height: '104px'}} className="mx-3 relative"></div>
+          <div className="right flex">
+            <p className=''><strong>Abhinandan</strong> Wins Over <strong>Monika</strong> and got <strong>100</strong> points</p>
+          </div>
+        </div>
+        </div>
+        <div className="card md:w-[80%] w-[100%] bg-gray-800 rounded-2xl my-2 cursor-pointer">
+        <div style={{alignItems: 'center'}} className="update p-4 flex 2xl:items-start w-full text-white hover:rounded-2xl hover:bg-gray-700">
+          <div className="left flex flex-col">
+            <img src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="Abhi" className='h-10 w-10 rounded-full object-cover' />
+            <p className='ml-2'>vs</p>
+            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" width={50} alt="Monika" className='h-10 w-10 rounded-full object-cover' />
+          </div>
+          <div style={{borderLeft: '2px solid #8080805e', height: '104px'}} className="mx-3 relative"></div>
+          <div className="right flex">
+            <p className=''><strong>Abhinandan</strong> Wins Over <strong>Monika</strong> and got <strong>100</strong> points</p>
+          </div>
+        </div>
+        </div>
+        <div className="card md:w-[80%] w-[100%] bg-gray-800 rounded-2xl my-2 cursor-pointer">
+        <div style={{alignItems: 'center'}} className="update p-4 flex 2xl:items-start w-full text-white hover:rounded-2xl hover:bg-gray-700">
+          <div className="left flex flex-col">
+            <img src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="Abhi" className='h-10 w-10 rounded-full object-cover' />
+            <p className='ml-2'>vs</p>
+            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" width={50} alt="Monika" className='h-10 w-10 rounded-full object-cover' />
+          </div>
+          <div style={{borderLeft: '2px solid #8080805e', height: '104px'}} className="mx-3 relative"></div>
+          <div className="right flex">
+            <p className=''><strong>Abhinandan</strong> Wins Over <strong>Monika</strong> and got <strong>100</strong> points</p>
+          </div>
+        </div>
         </div>
         
       </div>
