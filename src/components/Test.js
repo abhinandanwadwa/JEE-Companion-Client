204e7a093b46834b7d869654857e2a2b7e2d46be
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-15 10:50:18 +0530

    Removing unnecessary imports

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Test = () => {
     const navigate = useNavigate();
